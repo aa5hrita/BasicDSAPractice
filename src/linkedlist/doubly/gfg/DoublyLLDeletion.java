3a48e7cac78d1efcdb37d3942751fe327ac549f9
AASHRITA BILIANGADI <aashritavb@gmail.com>
2024-07-08 22:03:52 +0530

    DLL: insert and delete

---
+++ src/linkedlist/doubly/gfg/DoublyLLDeletion.java
@@ -0,0 +1,57 @@
+package linkedlist.doubly.gfg;
+
+public class DoublyLLDeletion {
+    static Node deleteAtBeg(Node head) {
+        head = head.next;
+        head.prev = null;
+        return head;
+    }
+
+    static void deleteAtPosition(Node curr, int position) {
+        int count = 0;
+
+        while (curr != null && count != position) {
+            curr = curr.next;
+            count++;
+        }
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+    }
+
+    static void deleteAtEnd(Node head) {
+        while (head.next != null) {
+            head = head.next;
+        }
+        head.prev.next = null;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node first = new Node(11);
+        Node second = new Node(12);
+        Node third = new Node(13);
+        Node fourth = new Node(14);
+        Node fifth = new Node(15);
+        Node sixth = new Node(16);
+
+        head.next = first;
+        first.prev = head;
+        first.next = second;
+        second.prev = first;
+        second.next = third;
+        third.prev = second;
+        third.next = fourth;
+        fourth.prev = third;
+        fourth.next = fifth;
+        fifth.prev = fourth;
+        fifth.next = sixth;
+        sixth.prev = fifth;
+
+        head = deleteAtBeg(head);
+        deleteAtPosition(head, 3);
+        deleteAtEnd(head);
+
+        System.out.println("Forward Traversal -> ");
+        DoublyLLTraversal2ways.forwardTraversal(head);
+    }
+}
---
+++ src/linkedlist/doubly/gfg/DoublyLLInsertion.java
@@ -0,0 +1,68 @@
+package linkedlist.doubly.gfg;
+
+public class DoublyLLInsertion {
+
+    static void insertEnd(Node head, int value) {
+        Node newNode = new Node(value);
+        Node current = head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        newNode.prev = current;
+
+    }
+
+    static Node insertBeginning(Node head, int value) {
+
+        Node newNode = new Node(value);
+        head.prev = newNode;
+        newNode.next = head;
+
+        return newNode;
+    }
+
+    static void insertAtPosition(Node head, int position) {
+        int count = 0;
+        while (head != null && count != position) {
+            count++;
+            head = head.next;
+        }
+        Node newNode = new Node(444);
+        newNode.prev = head.prev;
+        newNode.next = head;
+        head.prev.next = newNode;
+        head.prev = newNode;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node first = new Node(11);
+        Node second = new Node(12);
+        Node third = new Node(13);
+        Node fourth = new Node(14);
+        Node fifth = new Node(15);
+        Node sixth = new Node(16);
+
+        head.next = first;
+        first.prev = head;
+        first.next = second;
+        second.prev = first;
+        second.next = third;
+        third.prev = second;
+        third.next = fourth;
+        fourth.prev = third;
+        fourth.next = fifth;
+        fifth.prev = fourth;
+        fifth.next = sixth;
+        sixth.prev = fifth;
+
+        head = insertBeginning(head, 1010);
+        insertAtPosition(head, 4);
+        insertEnd(head, 8888);
+
+        System.out.println("Forward Traversal -> ");
+        DoublyLLTraversal2ways.forwardTraversal(head);
+    }
+}
