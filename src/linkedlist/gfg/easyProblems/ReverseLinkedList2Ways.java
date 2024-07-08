cd7740fb00a9162eb0a86766f0cd4d7d4c86600f
AASHRITA BILIANGADI <aashritavb@gmail.com>
2024-07-08 23:10:16 +0530

    Code commit

---
+++ src/linkedlist/gfg/easyProblems/ReverseLinkedList2Ways.java
@@ -0,0 +1,34 @@
+package linkedlist.gfg.easyProblems;
+
+public class ReverseLinkedList2Ways {
+
+    public static void main(String[] args) {
+        ReverseLinkedList2Ways x = new ReverseLinkedList2Ways();
+        SLLNode head = new SLLNode(111);
+        head = x.reverseSLL(head);
+    }
+
+    /*
+    Approach 1: Reverse a linked list by Iterative Method:
+    */
+    SLLNode reverseSLL(SLLNode head) {
+        SLLNode prev = null;
+        SLLNode current = head;
+        SLLNode next;
+
+        while (current != null) {
+
+            next = current.next; // move next pointer
+            current.next = prev; // for backward traversal
+            prev = current; // move prev pointer
+            current = next; //move current pointer , now current=next
+
+        }
+        head = prev; // when current=null, prev -> tail node = our new head
+        return head;
+    }
+    /*
+    Approach 2: Reverse a linked list using recursion
+    */
+
+}
---
+++ src/linkedlist/gfg/easyProblems/SLLNode.java
@@ -0,0 +1,11 @@
+package linkedlist.gfg.easyProblems;
+
+public class SLLNode {
+    int data;
+    SLLNode next;
+
+    public SLLNode(int x) {
+        this.data = x;
+        this.next = null;
+    }
+}
