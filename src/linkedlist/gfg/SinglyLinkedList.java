f7e32beadc9947d62fc6bc4e20611f5659b36a87
AASHRITA BILIANGADI <aashritavb@gmail.com>
2024-07-08 10:14:51 +0530

    master2

--- src/linkedlist/gfg/SinglyLinkedList.java
+++ src/linkedlist/gfg/SinglyLinkedList.java
@@ -2,6 +2,7 @@ package linkedlist.gfg;
 
 public class SinglyLinkedList {
     public static void main(String[] args) {
-        System.out.println(" This is in master");
+        System.out.println("This is in master");
+        System.out.println("checking");
     }
 }
