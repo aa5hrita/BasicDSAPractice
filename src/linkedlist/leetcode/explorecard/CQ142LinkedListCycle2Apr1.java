ed3485740c450ba0998ac7036c00d403a5789475
AASHRITA BILIANGADI <aashritavb@gmail.com>
2024-07-10 11:57:04 +0530

    CQ142

---
+++ src/linkedlist/leetcode/explorecard/CQ142LinkedListCycle2Apr1.java
@@ -0,0 +1,41 @@
+package linkedlist.leetcode.explorecard;
+
+import java.util.HashSet;
+
+public class CQ142LinkedListCycle2Apr1 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            // Initialize an empty hash set
+            HashSet<ListNode> nodesSeen = new HashSet<>();
+
+            // Start from the head of the linked list
+            ListNode node = head;
+
+            while (node != null) {
+                // If the current node is in nodesSeen, we have a cycle
+                if (nodesSeen.contains(node)) {
+                    return node;
+                } else {
+                    // Add this node to nodesSeen and move to the next node
+                    nodesSeen.add(node);
+                    node = node.next;
+                }
+            }
+
+            // If we reach a null, there is no cycle
+            return null;
+        }
+    }
+
+
+}
---
+++ src/linkedlist/leetcode/explorecard/CQ142LinkedListCycle2Apr2.java
@@ -0,0 +1,7 @@
+package linkedlist.leetcode.explorecard;
+
+//Approach 2: Floyd's Tortoise and Hare Algorithm
+//This algorithm is very difficult to derive on your own
+//and you would not be expected to do so in an interview without any help.
+public class CQ142LinkedListCycle2Apr2 {
+}
